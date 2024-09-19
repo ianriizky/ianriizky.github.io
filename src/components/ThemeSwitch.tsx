ae1569ba2165a3f0c8d1922dfad4f9394e9bed48
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2024-09-20 06:40:27 +0700

    feat: change theme switch component from menu into button

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,20 +1,10 @@
 "use client";
 
 import BlankIcon from "@/components/icons/Blank";
-import MonitorIcon from "@/components/icons/Monitor";
 import MoonIcon from "@/components/icons/Moon";
 import SunIcon from "@/components/icons/Sun";
-import {
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Radio,
-  RadioGroup,
-  Transition,
-} from "@headlessui/react";
 import { useTheme } from "next-themes";
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ThemeSwitch() {
   const [mounted, setMounted] = useState<boolean>(false);
@@ -25,87 +15,32 @@ export default function ThemeSwitch() {
   }, []);
 
   return (
-    <div className="mr-5 flex items-center">
-      <Menu as="div" className="relative inline-block text-left">
-        <div className="flex items-center justify-center hover:text-primary-500 dark:hover:text-primary-400">
-          <MenuButton aria-label="Theme switcher">
-            {mounted ? (
-              resolvedTheme === "dark" ? (
-                <MoonIcon />
-              ) : (
-                <SunIcon />
-              )
-            ) : (
-              <BlankIcon />
-            )}
-          </MenuButton>
-        </div>
-
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <MenuItems className="absolute right-0 z-50 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-800">
-            <RadioGroup value={theme} onChange={setTheme}>
-              <div className="p-1">
-                <Radio value="light">
-                  <MenuItem>
-                    {({ focus }) => (
-                      <button
-                        className={`${
-                          focus && "bg-primary-600 text-white"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        <div className="mr-2">
-                          <SunIcon />
-                        </div>
-                        Light
-                      </button>
-                    )}
-                  </MenuItem>
-                </Radio>
-                <Radio value="dark">
-                  <MenuItem>
-                    {({ focus }) => (
-                      <button
-                        className={`${
-                          focus && "bg-primary-600 text-white"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        <div className="mr-2">
-                          <MoonIcon />
-                        </div>
-                        Dark
-                      </button>
-                    )}
-                  </MenuItem>
-                </Radio>
-                <Radio value="system">
-                  <MenuItem>
-                    {({ focus }) => (
-                      <button
-                        className={`${
-                          focus && "bg-primary-600 text-white"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        <div className="mr-2">
-                          <MonitorIcon />
-                        </div>
-                        System
-                      </button>
-                    )}
-                  </MenuItem>
-                </Radio>
-              </div>
-            </RadioGroup>
-          </MenuItems>
-        </Transition>
-      </Menu>
-    </div>
+    <button
+      aria-label="Toggle Dark Mode"
+      type="button"
+      className="ml-1 mr-1 h-8 w-8 rounded p-1 sm:ml-4"
+      onClick={() =>
+        setTheme(
+          theme === "dark" || resolvedTheme === "dark" ? "light" : "dark",
+        )
+      }
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="text-gray-900 dark:text-gray-100"
+      >
+        {mounted ? (
+          theme === "dark" || resolvedTheme === "dark" ? (
+            <MoonIcon />
+          ) : (
+            <SunIcon />
+          )
+        ) : (
+          <BlankIcon />
+        )}
+      </svg>
+    </button>
   );
 }
