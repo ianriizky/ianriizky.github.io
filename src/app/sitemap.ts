47b196ac1be81e6fa4bfc115f811c0d7c9769bec
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2024-10-29 04:39:36 +0700

    fix: set correct list route on sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,12 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ["", "blog", "projects", "certificates"].map((route) => ({
-    url: `${process.env.APP_URL}/${route}`,
-    lastModified: new Date().toISOString().split("T")[0],
-  }));
+  const routes = ["", "blog", "project", "certificate", "resume", "about"].map(
+    (route) => ({
+      url: `${process.env.APP_URL}/${route}`,
+      lastModified: new Date().toISOString().split("T")[0],
+    }),
+  );
 
   return routes;
 }
